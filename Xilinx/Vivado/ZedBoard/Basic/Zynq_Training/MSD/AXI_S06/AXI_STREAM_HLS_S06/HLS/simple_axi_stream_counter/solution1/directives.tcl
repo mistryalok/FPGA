466186c309b54ffe020a01dcb880b00f209834b3
mistryalok <alokn73@gmail.com>
2016-03-29 18:14:26 +0530

    Moved Zedboard repo

--- Xilinx/Vivado/ZedBoard/Basic/Zynq_Training/MSD/AXI_S06/AXI_STREAM_HLS_S06/HLS/simple_axi_stream_counter/solution1/directives.tcl
+++
@@ -1,6 +0,0 @@
-############################################################
-## This file is generated automatically by Vivado HLS.
-## Please DO NOT edit it.
-## Copyright (C) 2014 Xilinx Inc. All rights reserved.
-############################################################
-set_directive_interface -mode axis "axi_stream_counter" counter
